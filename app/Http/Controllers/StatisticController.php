f8dc12ec912c0a3f89af324cf6db4b37165da98f
Ricardo Fontoura <ricardfo@usp.br>
2024-09-20 15:23:19 -0300

    ajustando a consulta.

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -15,66 +15,67 @@ class StatisticController extends Controller
     public function index(Request $request)
     {
         $this->authorize('admin');
-        $instance = Instance::count('id');
-        $record = Record::count('id');
 
-        if(isset($request->busca)){
-            $materiais = Record::join('instances','records.id','instances.record_id')
-            ->where('records.titulo','like','%'.$request->busca.'%')
-            ->orwhere('records.isbn',$request->busca)
-            ->toBase()
+        $query = Record::join('instances','records.id','instances.record_id')
             ->orderBy('records.created_at','desc')
-            ->paginate(15);
-        }else{
-            $materiais = Record::join('instances','records.id','instances.record_id')
-            ->toBase()
-            ->orderBy('records.created_at','desc')
-            ->paginate(15);
-        }
+            ->toBase();
+
+        $query->when($request->busca, function ($query) use ($request) {
+                $query->where('records.titulo','like','%'.$request->busca.'%')
+                    ->orwhere('records.isbn',$request->busca);
+        });
+
         return view('statistic.show')->with([
-            'instance' => $instance,
-            'record'   => $record,
-            'materiais' => $materiais,
+            'instance' => Instance::count('id'),
+            'record'   => Record::count('id'),
+            'materiais' => $query->paginate(15),
         ]);
     }
-    
+
     //issue: está repetindo o material
     public function exportarMaterial(Excel $excel, Record $record, Request $request){
-        $records = Record::select($record::camposMateriais())
-        ->where('records.titulo','like','%'.$request->busca.'%')
-        ->orwhere('records.isbn',$request->busca)
-        ->toBase()
-        ->get()
-        ->toArray();
+        $query = Record::select($record::camposMateriais());
+
+        $query->when($request->busca, function ($query) use ($request) {
+            $query->where('records.titulo','like','%'.$request->busca.'%')
+                  ->orwhere('records.isbn',$request->busca);
+        });
+        $records = $query->toBase()->get()->toArray();
 
         $export = new ExcelExport($records, $record::camposMateriais());
         return $excel->download($export, "Materiais: $request->busca.xlsx");
-        
+
     }
 
     //exporta todos as instances com seus titulos
     public function exportarExemplares(Excel $excel, Instance $instance, Request $request){
-        $instances = Instance::join('records','records.id','instances.record_id')
-        ->select($instance::camposExemplares())
-        ->where('records.titulo','like','%'.$request->busca.'%')
-        ->orwhere('records.isbn',$request->busca)
-        ->orwhere('instances.tombo',$request->busca)
-        ->toBase()
-        ->get()
-        ->toArray();
+        $query = Instance::join('records','records.id','instances.record_id')
+            ->select($instance::camposExemplares());
+
+        $query->when($request->busca, function ($query) use ($request) {
+            $query->where('records.titulo','like','%'.$request->busca.'%')
+                ->orwhere('instances.tombo',$request->busca)
+                ->orwhere('records.isbn',$request->busca);
+        });
+        $instances = $query->toBase()->get()->toArray();
+
         $export = new ExcelExport($instances, $instance::camposExemplares());
         return $excel->download($export, "Exemplares: $request->busca.xlsx");
     }
 
     //lista records com join nas instances
     public function exportarMateriaisCompletos(Excel $excel, Record $record, Request $request){
-        $records = Record::join('instances','instances.record_id','records.id')
-        ->select($record::camposCompletos())
-        ->where('records.titulo','like','%'.$request->busca.'%')
-        ->orwhere('records.isbn',$request->busca)
-        ->orwhere('instances.tombo',$request->busca)
-        ->get()
-        ->toArray();
+        $query = Record::join('instances','instances.record_id','records.id')
+            ->select($record::camposCompletos());
+
+        $query->when($request->busca, function ($query) use ($request) {
+            $query->where('records.titulo','like','%'.$request->busca.'%')
+                ->orwhere('records.isbn',$request->busca)
+                ->orwhere('instances.tombo',$request->busca);
+        });
+
+        $records = $query->get()->toArray();
+
         $export = new ExcelExport($records, $record::camposCompletos());
         return $excel->download($export, "Materiais_Completos: $request->busca .xlsx");
     }
